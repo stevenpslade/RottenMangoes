0b194920464c59516d32fbc8e065ee74b667be4e
stevenpslade <stevenpslade@gmail.com>
2015-10-26 18:08:13 -0700

    sign up/log in added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def create
+        @user = User.new(user_params)
+
+        if @user.save
+          session[:user_id] = @user.id # auto log in
+          redirect_to movies_path
+        else
+          render :new
+        end
+      end
+
   protected
 
   def user_params
