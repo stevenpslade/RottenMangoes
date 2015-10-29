989d01c4cdb6cc2fa685589d308cdd5d6f389cf8
stevenpslade <stevenpslade@gmail.com>
2015-10-29 14:22:09 -0700

    users have profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,15 +15,19 @@ class UsersController < ApplicationController
   end
 
   def create
-        @user = User.new(user_params)
+    @user = User.new(user_params)
 
-        if @user.save
-          session[:user_id] = @user.id
-          redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
-        else
-          render :new
-        end
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   protected
 
